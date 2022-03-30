73009d4cf67cba315aac6dbc41203872882cb329
adir-jscode <adir.jscode@gmail.com>
2022-03-30 21:56:29 +0600

    responsive updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Footer.css';
 const Footer = () => {
     return (
-        <div>
+        <div className="footer-section">
             <h1>How useState works?</h1>
             <p>UseState is a hook. It returns a pair of variable. UseState used to determine the changes of a state variable. it's variable disappears when the function exits. </p>
             <h1>Props vs State</h1>
--- src/Components/ShowOrder/ShowOrder.js
+++ src/Components/ShowOrder/ShowOrder.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './ShowOrder.css';
+import { Acad } from '@heroicons/react/solid'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faTrash} from '@fortawesome/free-solid-svg-icons'
 
@@ -15,12 +17,13 @@ const ShowOrder = (props) => {
             </div>
             
             <div className="show-order-info">
-               <div>
+               <div className="text-info">
                         <h4>{ name}</h4>
                     <p>{balance}</p>
 
                     </div>
                 <div className="button-delete">
+                   
                     
                         <button className="delete-btn"onClick={() =>handleDelete(props.order)}>Delete</button>
                     </div>
